7482745e4c7e1a5113213b9cfa9f592619385a7c
astrosteveo <34114851+astrosteveo@users.noreply.github.com>
2026-01-31 00:01:51 -0600

    feat(editor): add RightSidebar container

---
+++ packages/editor/src/components/RightSidebar.tsx
@@ -0,0 +1,29 @@
+/*
+ * This file is part of Emergence Editor.
+ * Copyright (C) 2026 astrosteveo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { EntityList } from './EntityList';
+import { Inspector } from './Inspector';
+
+export function RightSidebar() {
+  return (
+    <div className="w-64 bg-editor-surface border-l border-editor-border flex flex-col overflow-y-auto">
+      <EntityList />
+      <Inspector />
+    </div>
+  );
+}
